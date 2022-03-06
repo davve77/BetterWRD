5239227d13b3ca013660f22507420e1686b2d719
david77 <69095207+davve77@users.noreply.github.com>
2022-03-06 23:46:52 +0200

    Delete src directory

--- src/features/lowercasePremium.js
+++
@@ -1,5 +0,0 @@
-// Name: Lowercased Premium Usernames
-// Desc: Disables forced username uppercase for users with Premium
-
-var premiumusers = document.getElementsByClassName('patron')
-for(var i=0, l=premiumusers.length; i<l; i++) {premiumusers[i].style.textTransform = 'none'}
